bd3aaac2827bd687639335d158c240f2fa584ef6
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-05-07 12:14:52 -0500

    added employeeDto

---
+++ src/main/java/candide/emsbackend/dto/EmployeeDto.java
@@ -0,0 +1,18 @@
+package candide.emsbackend.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+
+    private Long id;
+
+
+    private String firstName;
+
+
+    private String lastName;
+
+
+    private String email;
+}
